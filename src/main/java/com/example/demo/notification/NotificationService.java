1e5c2f91f76118b910ca1f11cf9a4849af05fbba
fj200 <folajinmiadewole@gmail.com>
2025-08-19 01:06:54 -0400

    Implemented Spring Modulith using events

--- src/main/java/com/example/demo/notification/NotificationService.java
+++ src/main/java/com/example/demo/notification/NotificationService.java
@@ -2,14 +2,15 @@ package com.example.demo.notification;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-
+import org.springframework.modulith.events.ApplicationModuleListener;
 
 @Slf4j
 @Service
 public class NotificationService {
 
-    public void createNotification(NotificationDTO notification){
-        log.info("Received notification by module dependency for product {} in date {} by {}.",
+    @ApplicationModuleListener
+    public void notificationEvent(NotificationDTO notification) {
+        log.info("Received notification by event for product {} in date {} by {}.",
                 notification.productName(),
                 notification.date(),
                 notification.format());
--- src/main/java/com/example/demo/product/ProductService.java
+++ src/main/java/com/example/demo/product/ProductService.java
@@ -3,23 +3,21 @@ package com.example.demo.product;
 import com.example.demo.notification.NotificationDTO;
 import com.example.demo.notification.NotificationService;
 import com.example.demo.product.internal.Product;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
 
 @Service
+@RequiredArgsConstructor
 public class ProductService {
     private final NotificationService notificationService;
     private final ApplicationEventPublisher events;
 
-    public ProductService(ApplicationEventPublisher events, NotificationService notificationService){
-        this.events = events;
-        this.notificationService = notificationService;
-    }
-
     public void create(Product product){
-        notificationService.createNotification(new NotificationDTO(new Date(), "SMS", product.getName()));
+        events.publishEvent(new NotificationDTO(new Date(), "SMS", product.getName()));
     }
 
 
